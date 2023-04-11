ea2f91dde335c32ac57b30be9c44cc99cdf55ad9
Kate Starovoit <52281733+KateStarovoit@users.noreply.github.com>
2023-04-10 22:18:16 -0400

    Create phase_field_solve.py

---
+++ phase_field_solve.py
@@ -0,0 +1,14 @@
+import math
+from fd_laplacian import *
+
+def m(T, alpha, g, Te):
+    val = np.zeros(len(T))
+    for i in range(len(val)):
+        val[i] = math.atan(g*(Te-T[i]))
+    M = alpha/math.pi*val
+    return M
+
+def phase_field_solve(T,p,h,alpha,g,Te,tay):
+    L = Fd_laplacian(h)
+    sol = scipy.sparse.linalg.spsolve(tay*L, -p-p*(1-p)*(p-0.5+m(T, alpha, g, Te)))
+    return sol
