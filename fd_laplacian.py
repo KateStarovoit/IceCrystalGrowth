d01a122661c7acdf8157ead574cb920d864fc15d
Kate Starovoit <52281733+KateStarovoit@users.noreply.github.com>
2023-04-10 22:18:38 -0400

    Create fd_laplacian.py

---
+++ fd_laplacian.py
@@ -0,0 +1,27 @@
+import scipy
+import numpy as np
+
+def get_index(i,j,n):
+    return int(i+j*n)
+
+def get_cell_indexes(i,j,h):
+     return get_index(i,j,1/h), get_index(i+1,j,1/h), get_index(i-1,j,1/h), get_index(i,j-1,1/h), get_index(i,j+1,1/h)
+
+def Fd_laplacian(h):
+    n = int(1/h)
+    L = scipy.sparse.csc_matrix((n*n, n*n),)
+
+    # constructing L and U
+    for i in range(0, n):
+        for j in range(0, n):
+            mid_index, right_index, left_index, bottom_index, top_index = get_cell_indexes(i, j, h)
+            L[(mid_index, mid_index)] = -4
+            if (i + 1) < n:
+                L[(mid_index, right_index)] = 1
+            if i > 0:
+                L[(mid_index, left_index)] = 1
+            if j > 0:
+                L[(mid_index, bottom_index)] = 1
+            if (j + 1) < n:
+                L[(mid_index, top_index)] = 1
+    return L
