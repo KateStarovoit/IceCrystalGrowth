cede5ac1ac5184841448323f3da9019e821c6511
Kate Starovoit <52281733+KateStarovoit@users.noreply.github.com>
2023-04-20 19:38:29 -0400

    Update phase_field_solve.py

--- phase_field_solve.py
+++ phase_field_solve.py
@@ -1,8 +1,24 @@
 from fd_laplacian import *
+import numpy as np
 
-def m(T, alpha, g, Te):
-    return alpha/np.pi*np.arctan(g * (Te - T))
+"""
+Phase field solve 
+Input
+T - temperature n x 1 array
+p - phase field at prev step. n x 1 array
+h - grid size
+alpha - constant
+g - constant
+Te - initial temperature
+t - constant
+Output
+Phase field update dp/dt
+"""
+def phase_field_solve(T,p,h,alpha,g,Te,tay, eps):
+    L = Fd_laplacian(h)
+    m = alpha/np.pi*np.arctan(g * (Te - T))
 
-def phase_field_solve(T,p,h,alpha,g,Te,t):
-    L = Fd_laplacian_dense(h)
-    return (np.dot(L,p)+np.dot(p,(1-p))*(p-0.5+m(T, alpha, g, Te)))/t
+    diffuse_term = eps*eps*L@p
+    sourse_term = (p)*(1-p)*(p-0.5+m)
+    p_ = (diffuse_term+sourse_term)/tay
+    return p_
