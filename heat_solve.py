61a17d5eedd8c5c6720d097bf300e2bde493cf99
Kate Starovoit <52281733+KateStarovoit@users.noreply.github.com>
2023-04-20 19:38:07 -0400

    Update heat_solve.py

--- heat_solve.py
+++ heat_solve.py
@@ -6,9 +6,9 @@ Input
 alpha - constant
 T - previous T
 p - phase field
-K - constant
+K - latent heat constant
 h - grid size
 """
-def heat_solve(alpha, T, p, k, h):
-    L = Fd_laplacian_dense(h)
-    return alpha*alpha*np.dot(L, T)+k*p
+def heat_solve(a, T, p, k, h):
+    L = Fd_laplacian(h)
+    return a*a*L@T + k * p
